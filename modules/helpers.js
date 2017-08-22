2b77877423fd0b32b6b896023c6c0fdb86794848
ian <zyktnn@gmail.com>
2017-08-22 10:47:11 +0300

    Added Request helper

---
+++ modules/helpers.js
@@ -0,0 +1,25 @@
+function Request(url) {
+    var _url = url
+
+    var request = {
+        url: _url
+    }
+
+    request.onSuccess = function(callback) {
+        request._success = callback
+        return request
+    }
+
+    request.send = function() {
+        var doc = new XMLHttpRequest();
+        doc.onreadystatechange = function() {
+            if (doc.readyState == XMLHttpRequest.DONE) {
+                request._success(doc.responseText)
+            }
+        }
+        doc.open("GET", url)
+        doc.send()
+    }
+
+    return request
+}
